ef6fed96a63677195050049d0831e96760048b40
yujaemin <woawoa0513@gmail.com>
2022-07-07 18:28:13 +0900

    20220707 section6

--- test.java
+++ test.java
@@ -1,5 +1,15 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class test {
     public static void main(String[] args) {
-        System.out.println("테스트 메시지");
+        Queue<Integer> q = new LinkedList<>(); // int형 queue 선언
+
+        q.offer(3);
+        q.offer(5);
+//        q.offer(1);
+//        q.offer(4);
+        System.out.println(q.poll());
+        System.out.println(q.remove());
     }
 }
